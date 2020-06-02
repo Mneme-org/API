cb9738ab82f1be3847e4361c74f231d9e9b3373a
Spyros Roum <spyrosr1@gmail.com>
2020-06-03 00:53:51 +0300

    Stop using flask-restful

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
-api_ = Api(app)
 
 from server.api import routes
