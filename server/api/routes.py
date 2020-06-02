732dcce05e851645d3378a22a93cead2b745847e
Spyros Roum <spyrosr1@gmail.com>
2020-06-03 02:06:29 +0300

    Add user creation

--- server/api/routes.py
+++ server/api/routes.py
@@ -1,9 +1,12 @@
-from flask import request, make_response, jsonify
-from server.api import app
+import uuid
+
+from server.api import app, db
 from server.api.models import User
 from server.api.utils import generate_auth_token
-
 from werkzeug.security import check_password_hash
+
+from flask import request, make_response, jsonify
+
 
 @app.route('/login', methods=["GET"])
 def login():
@@ -12,16 +15,33 @@ def login():
     if not auth or not auth.username or not auth.password:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic-realm="Login required"'})
 
-    user = User.query.filter_by(user_name=auth.username).first()
+    user = User.query.filter_by(username=auth.username).first()
 
     if not user:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic-realm="Login required"'})
 
-    if not check_password_hash(user.password, auth.password):
+    # Because the api will probably be receiving already hashed passwords
+    # This is not final
+    if user.password == auth.password:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic-realm="Login required"'})
 
     token = generate_auth_token(user.public_id)
     return jsonify({'token': token})
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    print(request.get_json())
+    print(request.data)
+    data = request.get_json(force=True)
+
+    public_id = str(uuid.uuid4())
+    user = User(public_id=public_id, username=data['username'], password=data['password'])
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({'status': '200'})
 
 
 if __name__ == '__main__':
