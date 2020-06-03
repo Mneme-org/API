ccd39fe3432de571f960b8adbba37d46b7bcd837
Spyros Roum <spyrosr1@gmail.com>
2020-06-03 17:16:44 +0300

    Add util for needing token and getting user from it

--- server/api/utils.py
+++ server/api/utils.py
@@ -1,7 +1,10 @@
 from datetime import datetime, timedelta
+from functools import wraps
 
 from server.api import app
+from server.api.models import User
 
+from flask import request, jsonify
 import jwt
 
 
@@ -19,3 +22,24 @@ def generate_auth_token(pub_id):
         return str(token)
     except Exception as e:
         return e
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('x-access-token', None)
+
+        if token is None:
+            return jsonify({'message': 'Token is required!'}), 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            user = User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message': 'Token is invalid'}), 401
+
+        if not user:
+            return jsonify({'message': 'Token is invalid'}), 401
+
+        return f(user, *args, **kwargs)
+
+    return decorated
