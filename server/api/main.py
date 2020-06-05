5dcc8a2c771cd3e4748db67b9a17ccf9e626424a
Spyros Roum <spyrosr1@gmail.com>
2020-06-05 17:33:39 +0300

    Add routes for creating and reading all journals, also rename login to token

--- server/api/main.py
+++ server/api/main.py
@@ -2,7 +2,7 @@ from typing import List
 from datetime import timedelta
 
 from fastapi import Depends, HTTPException, status
-from fastapi.security import HTTPBasicCredentials
+from fastapi.security import HTTPBasicCredentials, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from . import app, security, ACCESS_TOKEN_EXPIRE_MINUTES
@@ -11,9 +11,10 @@ from .database import engine
 
 models.Base.metadata.create_all(bind=engine)
 
-@app.get("/login", response_model=schemas.Token)
-def login(db: Session = Depends(utils.get_db), credentials: HTTPBasicCredentials = Depends(security)):
-    user = utils.auth_user(credentials.username, credentials.password, db)
+
+@app.post("/token", response_model=schemas.Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(utils.get_db)):
+    user = utils.auth_user(db, form_data.username, form_data.password)
 
     if not user:
         raise HTTPException(
@@ -31,10 +32,28 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(utils.get_db)):
     db_user = crud.get_user_by_username(db, name=user.username)
     if db_user:
         raise HTTPException(status_code=400, detail="Username already registered")
-    return crud.create_user(db=db, user=user)
+    else:
+        return crud.create_user(db=db, user=user)
 
 
 @app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(utils.get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
+
+
+@app.post('/journals/', response_model=schemas.Journal)
+def create_journal(jrnl: schemas.JournalCreate, user: models.User = Depends(utils.get_current_user),
+                   db: Session = Depends(utils.get_db)):
+    db_jrnl = crud.get_jrnl(db, user.public_id, jrnl)
+    if db_jrnl:
+        raise HTTPException(status_code=400, detail="This journal already exists for this user")
+    else:
+        return crud.create_journal(db, user.public_id, jrnl)
+
+
+@app.get("/journals/", response_model=List[schemas.Journal])
+def read_journals(skip: int = 0, limit: int = 100, user: models.User = Depends(utils.get_current_user),
+                  db: Session = Depends(utils.get_db)):
+    jrnls = crud.get_journals_for(db, user, skip=skip, limit=limit)
+    return jrnls
