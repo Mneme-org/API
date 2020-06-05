d871a41f2a687f0827a33cd5ae92fdae518390b1
Spyros Roum <spyrosr1@gmail.com>
2020-06-05 16:44:00 +0300

    Add TokenData base schema

--- server/api/schemas.py
+++ server/api/schemas.py
@@ -56,3 +56,6 @@ class Token(BaseModel):
     access_token: str
     # Give access to the person who has that token, no questions asked
     token_type: str = "bearer"
+
+class TokenData(BaseModel):
+    public_id: str
