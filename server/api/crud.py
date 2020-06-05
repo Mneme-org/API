093592dda5b2f67cc97df6b666f91f7a85d3b713
Spyros Roum <spyrosr1@gmail.com>
2020-06-05 16:57:52 +0300

    Import the needed models

--- server/api/crud.py
+++ server/api/crud.py
@@ -3,27 +3,28 @@ from typing import Optional
 
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from . import schemas
+from .models import User
 from . import pwd_context
 
 
-def get_user_by_pub_id(db: Session, pub_id: str) -> Optional[models.User]:
-    return db.query(models.User).filter(models.User.public_id == pub_id).first()
+def get_user_by_pub_id(db: Session, pub_id: str) -> Optional[User]:
+    return db.query(User).filter(User.public_id == pub_id).first()
 
 
-def get_user_by_username(db: Session, name: str) -> Optional[models.User]:
-    return db.query(models.User).filter(models.User.username == name).first()
+def get_user_by_username(db: Session, name: str) -> Optional[User]:
+    return db.query(User).filter(User.username == name).first()
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
+    return db.query(User).offset(skip).limit(limit).all()
 
 
-def create_user(db: Session, user: schemas.UserCreate) -> models.User:
+def create_user(db: Session, user: schemas.UserCreate) -> User:
     hashed_password = pwd_context.hash(user.password)
 
     pub_id = str(uuid.uuid4())
-    new_user = models.User(username=user.username, hashed_password=hashed_password, public_id=pub_id)
+    new_user = User(username=user.username, hashed_password=hashed_password, public_id=pub_id)
 
     db.add(new_user)
     db.commit()
