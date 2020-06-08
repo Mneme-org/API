0c829697758aecb9794c2de0919aa761345aed8c
Spyros Roum <spyrosr1@gmail.com>
2020-06-08 15:25:48 +0300

    Add crud for getting entries

--- server/api/crud/entries.py
+++ server/api/crud/entries.py
@@ -4,8 +4,8 @@ from server.api import schemas
 from server.api.models.models import Entry, Keyword
 
 
-def create_entry(db: Session, entry: schemas.EntryCreate) -> Entry:
-    new_entry = Entry(jrnl_id=entry.jrnl_id, short=entry.short, long=entry.long, date=entry.date)
+def create_entry(db: Session, entry: schemas.EntryCreate, jrnl_id: int) -> Entry:
+    new_entry = Entry(jrnl_id=jrnl_id, short=entry.short, long=entry.long, date=entry.date)
 
     db.add(new_entry)
     db.commit()
@@ -18,3 +18,7 @@ def create_entry(db: Session, entry: schemas.EntryCreate) -> Entry:
         db.refresh(new_kw)
 
     return new_entry
+
+
+def get_entry_by_id(db: Session, entry_id: int) -> Entry:
+    return db.query(Entry).filter(Entry.id == entry_id).first()
