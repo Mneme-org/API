e4e2724729f32ad2e6d37f650b146d4271b15478
Spyros Roum <spyrosr1@gmail.com>
2020-06-14 00:58:18 +0300

    Move the `app` definition to `main.py`
    Test user creation
    
    Add pytest and requests to dev dependencies
    
    Ignore test.db
    
    Add tests
    
    Add tests

---
+++ server/api/tests/test_main.py
@@ -0,0 +1,149 @@
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from ..database import Base
+from ..main import app
+from ..utils import get_db
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+client = TestClient(app)
+
+
+def test_create_user():
+    r = client.post(
+        "/users/",
+        json={"username": "test1", "password": "12345"}
+    )
+    print(r.text)
+    assert r.status_code == 200
+    assert r.text is not None
+    data = r.json()
+    assert data["username"] == "test1"
+    assert data["journals"] == []
+    assert "public_id" in data
+    user_id = data["public_id"]
+
+    r = client.get(
+        "/users/"
+    )
+    assert r.status_code == 200
+    assert r.text is not None
+    data = r.json()
+    assert user_id in [user["public_id"] for user in data]
+
+
+def log_in():
+    r = client.post(
+        "/token",
+        data={"username": "test1", "password": "12345"}
+    )
+    data = r.json()
+    return "Bearer " + data["access_token"]
+
+
+def test_create_jrnl():
+    token = log_in()
+
+    r = client.post(
+        "/journals/",
+        headers={"Authorization": token},
+        json={
+            "name": "journal_1"
+        }
+    )
+    print(r.text)
+
+    assert r.status_code == 200
+    data = r.json()
+    assert data["name"] == "journal_1"
+    assert data["entries"] == []
+
+
+def test_read_journals():
+    token = log_in()
+
+    r = client.get(
+        "/journals/",
+        headers={"Authorization": token},
+    )
+
+    assert r.status_code == 200
+    data = r.json()
+    assert "journal_1" in [jrnl["name"] for jrnl in data]
+
+
+def test_read_journal():
+    token = log_in()
+
+    r = client.get(
+        "/journals/journal_1",
+        headers={"Authorization": token},
+    )
+    assert r.status_code == 200
+    data = r.json()
+    assert data["name"] == "journal_1"
+    assert data["entries"] == []
+
+
+def test_create_entry_with_long():
+    """Create an entry with no keywords"""
+    token = log_in()
+
+    r = client.post(
+        "/journals/journal_1/entries/",
+        headers={"Authorization": token},
+        json={
+            "short": "entry_1",
+            "long": "a long text",
+            "date": "now",
+            "keywords": []
+        }
+    )
+
+    assert r.status_code == 200
+    data = r.json()
+    assert data["short"] == "entry_1"
+    assert data["long"] == "a long text"
+    assert data["date"] == "now"
+    assert data["keywords"] == []
+
+
+def test_create_entry_with_keyword():
+    """Create an entry with keyword"""
+    token = log_in()
+
+    r = client.post(
+        "/journals/journal_1/entries/",
+        headers={"Authorization": token},
+        json={
+            "short": "entry_1",
+            "date": "now",
+            "keywords": [{"word": "a_keyword"}]
+        }
+    )
+
+    assert r.status_code == 200
+    data = r.json()
+    assert data["short"] == "entry_1"
+    assert data["date"] == "now"
+    assert data["keywords"][0]["word"] == "a_keyword"
+    assert data["id"] == data["keywords"][0]["entry_id"]
